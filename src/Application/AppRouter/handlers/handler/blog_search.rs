6ecf3d7249810b876e4e9f2963ce0962ea8eb058
andrian <krackedissad@gmail.com>
2023-07-22 18:45:50 +0300

    Fixed search bar bug

--- src/Application/AppRouter/handlers/handler/blog_search.rs
+++ src/Application/AppRouter/handlers/handler/blog_search.rs
@@ -31,9 +31,14 @@ pub async fn get_blog_search() -> Result<Html<String>, AppError>
     retreive_page_untouched("blog-search").await
 }
 
-pub async fn post_blog_search(Json(keyword): Json<Keyword>) -> Result<Json<Keyword>, AppError>
+pub async fn post_blog_search(Json(keyword): Json<Keyword>) -> String
 {
-    Ok(Json(keyword))
+    let error = r#"{"message": "No results"}"#;
+    match read_directory(keyword).await
+    {
+	Ok(v) => serde_json::to_string(&v).unwrap(),
+	Err(_e) => serde_json::from_str(error).unwrap(),
+    }
 }
 
 pub async fn read_directory(keyword: Keyword) -> Result<String, AppError>
@@ -48,7 +53,6 @@ pub async fn read_directory(keyword: Keyword) -> Result<String, AppError>
                 .file_name()
                 .and_then(|name| name.to_str())
                 .ok_or_else(||{AppError::BadRequest});
-
             // If the directory name matches the keyword value, return the last two entries of that category
 	    if let Ok(dir_name) = dir_name {
 		if dir_name == keyword.value
