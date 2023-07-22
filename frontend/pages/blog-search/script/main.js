bf28898cf99cebb60d7ffd7d706a3d1e28a31a58
andrian <krackedissad@gmail.com>
2023-07-22 17:13:56 +0300

    First implementation of search

--- frontend/pages/blog-search/script/main.js
+++ frontend/pages/blog-search/script/main.js
@@ -1,7 +1,3 @@
-  document.getElementById("kracked-web").addEventListener("click", function() {
-    window.location.href = "somewhere";
-  });
-
   document.getElementById("blog-search").addEventListener("click", function(){
        window.location.href = "blogsearch";
    });
@@ -10,7 +6,45 @@
        window.location.href = "cv";
   })
 
-
   document.getElementById("about-blog").addEventListener("click", function(){
        window.location.href = "blogabout";
-})
+  })
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("search-bar");
+
+    searchInput.addEventListener("input", debounce(handleSearch, 300));
+
+    function handleSearch() {
+      const keyword = searchInput.value;
+	const backendUrl = 'https://localhost:443/blogsearch';
+
+      fetch(backendUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ keyword }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+    }
+
+    function debounce(func, delay) {
+      let timer;
+      return function () {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+          func.apply(context, args);
+        }, delay);
+      };
+    }
+
+  });
--- src/Application/AppRouter/handlers/handler/blog_search.rs
+++ src/Application/AppRouter/handlers/handler/blog_search.rs
@@ -2,17 +2,27 @@
 use axum::
 {
     response::Html,
+    extract::Json,
 };
-
+//Serde
+use serde::{Deserialize, Serialize};
 //Custom Error
 use super::error::app_error::AppError;
+//Helper Funcs
+use super::helper_funcs::retreive_page_untouched;
+
+#[derive(Deserialize, Serialize)]
+pub struct Keyword
+{
+ value: String
+}
 
 pub async fn get_blog_search() -> Result<Html<String>, AppError>
 {
-    todo!()
+    retreive_page_untouched("blog-search").await
 }
 
-pub async fn post_blog_search() -> Result<Html<String>, AppError>
+pub async fn post_blog_search(Json(keyword): Json<Keyword>) -> Result<Json<Keyword>, AppError>
 {
-    todo!()
+    Ok(Json(keyword))
 }
