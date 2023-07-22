bba041199e997089a5e3f5191f1ed67d1757685c
andrian <krackedissad@gmail.com>
2023-07-22 15:21:44 +0300

    Added more redirects

--- frontend/pages/home/script/main.js
+++ frontend/pages/home/script/main.js
@@ -1,3 +1,11 @@
   document.getElementById("kracked-web").addEventListener("click", function() {
     window.location.href = "somewhere";
   });
+
+  document.getElementById("blog-search").addEventListener("click", function(){
+       window.location.href = "blogsearch";
+   });
+
+  document.getElementById("cv").addEventListener("click", function(){
+       window.location.href = "cv";
+})
