4471ba95517831310806421e0013f8784b45332d
mrandr1an <krackedissad@gmail.com>
2024-06-28 09:54:29 +0300

    Added static file services for bootstrap prism and the css styles

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod parser;
 mod services;
-
 use axum::{routing::get, routing::post, Router};
 use services::{blog::blog, home::home, publish::publish};
+use tower_http::services::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
@@ -10,7 +10,23 @@ async fn main() {
     let routes = Router::new()
         .route("/", get(home))
         .route("/blog/:blog_title", get(blog))
-        .route("/publish", post(publish));
+        .route("/publish", post(publish))
+        .route_service(
+            "/blog/dependencies/css/styler.css",
+            ServeFile::new("src/theme/dependencies/css/styler.css"),
+        )
+        .route_service(
+            "/blog/dependencies/css/bootstrap.min.css",
+            ServeFile::new("src/theme/dependencies/css/bootstrap.min.css"),
+        )
+        .route_service(
+            "/blog/dependencies/js/bootstrap.min.js",
+            ServeFile::new("src/theme/dependencies/js/bootstrap.min.js"),
+        )
+        .route_service(
+            "/blog/dependencies/js/prism.js",
+            ServeFile::new("src/theme/dependencies/js/prism.js"),
+        );
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, routes).await.unwrap();
